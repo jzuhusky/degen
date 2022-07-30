58ab0b2c439d204283f8f0f7c761177bb996ea1c
jzuhusky <jzuhusky@gmail.com>
2022-07-30 18:41:10 -0400

    Add tox, setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="degen",
+    version="0.1.0",
+    description="A clean way to work with betting odds",
+    author="Joey Zuhusky",
+    keywords='odds sportsbetting betting python betting gambling',
+    author_email="jzuhusky@gmail.com",
+    maintainer_email="jzuhusky@gmail.com",
+    url="https://github.com/jzuhusky/degen",
+)
