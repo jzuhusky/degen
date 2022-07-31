6a7af8f5973663b49d565e1eb0a17d0f85b98994
jzuhusky <jzuhusky@gmail.com>
2022-07-31 11:00:53 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
+from pathlib import Path
 from setuptools import setup
 
-with open("requirements.txt") as f:
-    required_pkgs = f.read().splitlines()
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="degen",
-    version="0.1.0",
+    version="0.1.1",
     description="Programming with betting odds, made simple",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Joey Zuhusky",
     keywords="odds sportsbetting betting python betting gambling",
     author_email="jzuhusky@gmail.com",
     maintainer_email="jzuhusky@gmail.com",
     url="https://github.com/jzuhusky/degen",
-    install_requires=required_pkgs,
+    install_requires=["pydantic>=1.9.1"],
     license="MIT",
 )
